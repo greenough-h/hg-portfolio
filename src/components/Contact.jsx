877d5922a90feb23786fbe81c31f87f03c220653
Hayley <37635523+greenough-h@users.noreply.github.com>
2024-01-22 16:23:16 -0800

    reordered contact info

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,10 +6,26 @@ export default function Contact() {
     <div className="Contact">
       <h2>Contact Information</h2>;
       <div className="contact-details">
+        <div className="email">
+          <h3>Email Me!</h3>
+          <a href="mailto:greenough.hayley@gmail.com">
+            greenough.hayley@gmail.com
+          </a>{" "}
+        </div>
         <div className="linkedIn">
           <h3>LinkedIn</h3>
           <a
             href="https://www.linkedin.com/in/hayley-greenough/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Profile
+          </a>
+        </div>
+        <div className="github">
+          <h3>Github</h3>
+          <a
+            href="https://github.com/greenough-h"
             target="_blank"
             rel="noreferrer"
           >
@@ -25,22 +41,6 @@ export default function Contact() {
           >
             Art Profile
           </a>
-        </div>
-        <div className="art-instagram">
-          <h3>Github</h3>
-          <a
-            href="https://github.com/greenough-h"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Profile
-          </a>
-        </div>
-        <div className="art-instagram">
-          <h3>Email Me!</h3>
-          <a href="mailto:greenough.hayley@gmail.com">
-            greenough.hayley@gmail.com
-          </a>{" "}
         </div>
       </div>
       <br />
