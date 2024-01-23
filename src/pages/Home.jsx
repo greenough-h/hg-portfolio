cc0f3679bbb662ac6fbacd6d10e04578d1016d9a
Hayley <37635523+greenough-h@users.noreply.github.com>
2024-01-22 16:35:28 -0800

    Fix header and footer links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,9 @@ export default function Home() {
         </Link>
       </header>
       <main>
-        <p className="mt-5">Take a look at some of my current work!</p>
+        <p className="mt-5">
+          Take a look at a few of the applications I've created here
+        </p>
         <CurrentWorkSection
           orient="image-left"
           title="Weather App"
@@ -38,11 +40,36 @@ export default function Home() {
         />
       </main>
       <ContactBanner />
+      <div>
+        <a href="mailto:greenough.hayley@gmail.com" className="email">
+          greenough.hayley@gmail.com
+        </a>
+      </div>
       <footer className="d-flex justify-content-center">
-        <div className="footLink">linkedin</div>
-        <div className="footLink">github</div>
-        <div className="footLink">instagram</div>
-        <div className="footLink">site</div>
+        <a
+          href="https://www.linkedin.com/in/hayley-greenough/"
+          target="_blank"
+          rel="noreferrer"
+          className="footLink"
+        >
+          linkedin
+        </a>
+        <a
+          href="https://github.com/greenough-h"
+          target="_blank"
+          rel="noreferrer"
+          className="footLink"
+        >
+          github
+        </a>
+        <a
+          href="https://www.instagram.com/hayleygreenough.art/"
+          target="_blank"
+          rel="noreferrer"
+          className="footLink"
+        >
+          instagram
+        </a>
       </footer>
     </div>
   );
