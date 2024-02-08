7572f36bc3725aec65f50a6e27a3ec9d0a25be24
Hayley <37635523+greenough-h@users.noreply.github.com>
2024-02-08 09:57:44 -0800

    Update Pinnacles description

--- src/components/SiteImage.jsx
+++ src/components/SiteImage.jsx
@@ -34,7 +34,7 @@ export default function SiteImage(props) {
       imgNameImport: pinnaclesImage,
       sitelink: "https://neon-pasca-10d87b.netlify.app/",
       description:
-        "This site is built with vanilla HTML and CSS and was a project to further my responsive web design skills. I Thoroughly enjoyed building htis one despite having learned all the skills to potentially make it more complex in React because it was so enjoyable to just create something that really resonated with what I'm passionate about - being outdoors and with my friends hiking and enjoying nature and this park is a really exceptional place to do just that.",
+        "This site is built with vanilla HTML and CSS and was a project to further my responsive web design skills. I thoroughly enjoyed building this site despite having learned all the skills to potentially make it more complex (cue the parade of potential React components). It was so enjoyable to just create something that really resonated with what I'm passionate about - being outdoors and with my friends hiking and enjoying nature. Also, this park is a really an exceptional place to do just that and it was fun to try and capture that with some of my own personal images and experiences.",
     },
   };
 
