66d37042b6eba59180db68a222863ad0eca1d2a9
Hayley <37635523+greenough-h@users.noreply.github.com>
2024-02-19 15:24:57 -0800

    Added image animation on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./About.css";
 import Contact from "../components/Contact";
+import { motion } from "framer-motion";
 
 export default function About() {
   return (
@@ -33,11 +34,17 @@ export default function About() {
           </p>
         </div>
         <div className="col-lg-6">
-          <img
-            src={require("../images/me.jpeg")}
-            alt="Headshot of Hayley Greenough"
-            className="img-fluid"
-          />
+          <motion.div
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.5 }}
+          >
+            <img
+              src={require("../images/me.jpeg")}
+              alt="Headshot of Hayley Greenough"
+              className="img-fluid"
+            />
+          </motion.div>
         </div>
       </div>
       <Contact />
